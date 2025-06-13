f416fab6dbab8512811725da1cd1b602672db186
snehagoyal25 <goyalsneha995@gmail.com>
2025-06-13 14:12:31 +0530

    end

--- config/database.js
+++ config/database.js
@@ -2,6 +2,23 @@ const mongoose = require ("mongoose");
 
 require("dotenv").config();
 
-const dbConnectc = ()=>{
-    mongoose.connect(process.env.DATABASE_URL)
+const dbConnect = ()=>{
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser : true,
+        useUnifiedTopology : true,
+    })
+
+
+.then(()=>{
+    console.log("Database connection is successful !")
+})
+
+.catch((error)=>{
+    console.log("Issue in Database connection !");
+    console.log(error.message);
+    process.exit(1);
+});
+
 }
+
+module.exports = dbConnect;
