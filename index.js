7cf0de08b560dc1277b2470e4562dd4d23465364
snehagoyal25 <goyalsneha995@gmail.com>
2025-06-14 08:51:16 +0530

    end

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require ("express");
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("App is running successfully ");
+// app.listen(3000,()=>{
+//     console.log("App is running successfully ");
+// })
+
+
+// instead of writing port number , the best practice is to load config from env file.
+
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+// need middleware to parse jso request body
+app.use(express.json());
+
+// import routes for TODO API 
+const todoRoutes = require("./routes/todos"); 
+
+// mount the todo API rountes 
+app.use("/api/v1",todoRoutes);
+
+// start server 
+app.listen(PORT,()=>{
+    console.log(`Server started successfullay at ${PORT} `);
 })
+
+// connect to the database
+const dbConnect = require("../config/database");
---
+++ routes/todos.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.router();
+
+// import controller  
+const {createTodo} = require("../controllers/createTodo");
+
+// define API routes
