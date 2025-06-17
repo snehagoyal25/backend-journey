acb63d0dab44d908b455dca1d9614850ddce3a98
snehagoyal25 <goyalsneha995@gmail.com>
2025-06-17 21:18:57 +0530

    end

--- controllers/getTodo.js
+++ controllers/getTodo.js
@@ -6,9 +6,22 @@ exports.getTodo = async(req,res) =>{
     try{
 // fetch all the todos items from the database .
         const todos = await Todo.find({});
+
+        res.status(200)
+        .json({
+            success : true,
+            data : todos,
+            message : "Entire todo data is fetched " ,
+        })
     }
 
     catch(err){
-
+        console.error(err);
+        res.status(500)
+        .json({
+            success : false ,
+            error : err.message,
+            message : "Server error",
+        })
     }
 }
