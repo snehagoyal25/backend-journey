caefe51927802181de175ebf9b0a6e912fbba78a
snehagoyal25 <goyalsneha995@gmail.com>
2025-06-20 08:09:02 +0530

    end

--- controllers/getTodo.js
+++ controllers/getTodo.js
@@ -25,3 +25,36 @@ exports.getTodo = async(req,res) =>{
         })
     }
 }
+
+
+exports.getTodoById = async(req,res) =>{
+try{
+    // extract todo basis on ID 
+    const id = req.params.id;
+    const todo = await Todo.findById({_id:id})
+
+    // data for given id is not found 
+    if(!todo){
+        return res.status(404).json({
+            success : false,
+            message :"No data found with the given Id",
+        })
+    }
+
+    // data for given ID is found 
+    res.status(200).json({
+        success : true,
+        data : todo,
+        message : `Todo ${id} data is successfully fetched `,
+    })
+
+}
+catch(err){
+    console.error(err);
+    res.status(500).json({
+        success : false ,
+        error : err.message ,
+        message : "server error",
+    });
+}
+}
--- routes/todos.js
+++ routes/todos.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 // import controller  
 const {createTodo} = require("../controllers/createTodo");
-const {getTodo} = require("../controllers/getTodo");
+const {getTodo,getTodoById} = require("../controllers/getTodo");
 
 // define API routes
 router.post("/createTodo",createTodo);  // for createTodo
-
 router.get("/getTodos",getTodo);  // for getTodo
+router.get("/getTodos/:id", getTodoById)  // for getTodo by ID 
 
 module.exports = router;
