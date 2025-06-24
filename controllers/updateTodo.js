a4ae6d29cf95a6bce3aada98f0ecbb41cbcfd880
snehagoyal25 <goyalsneha995@gmail.com>
2025-06-24 23:24:35 +0530

    end

---
+++ controllers/updateTodo.js
@@ -0,0 +1,25 @@
+// Update is a put type request.
+// Import the model as we need schema that is the description.
+const Todo =require("../models/Todo")
+
+// Define route handler 
+expots.updateTodo = async(req,res)=>{
+    try{
+// Extract title and description from the request body 
+const{id}= req.params
+const {title , description}= req.body ;
+        const todo = await Todo.findByIdAndUpdate(
+            {_id:id},
+            {title,description,updatedAt:Date.now()},
+        ) 
+        res.status(200).json({
+            success :  true,
+            data : todo,
+            message : `Updated Successfully `
+        })
+    }
+
+    catch(err){
+
+    }
+}
--- routes/todos.js
+++ routes/todos.js
@@ -4,10 +4,12 @@ const router = express.Router();
 // import controller  
 const {createTodo} = require("../controllers/createTodo");
 const {getTodo,getTodoById} = require("../controllers/getTodo");
+const{updateTodo} = require("../controllers/updateTodo")
 
 // define API routes
 router.post("/createTodo",createTodo);  // for createTodo
 router.get("/getTodos",getTodo);  // for getTodo
 router.get("/getTodos/:id", getTodoById)  // for getTodo by ID 
+router.put("/updateTodo/:id",updateTodo) // for updateTodo
 
 module.exports = router;
